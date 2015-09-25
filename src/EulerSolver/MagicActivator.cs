65d1003de5dd5f37283253b9caad1eedb46427d0
Cussa Mitre <cussa.mitre@gmail.com>
2015-09-25 15:33:14 -0300

    Start of Euler Problems

---
+++ src/EulerSolver/MagicActivator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolver
+{
+    static class MagicActivator
+    {
+        public static IEulerProblem MagicallyCreateInstance(string className)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var type = assembly.GetTypes().FirstOrDefault(t => t.Name == className);
+            if (type == null)
+                return null;
+
+            return (IEulerProblem)Activator.CreateInstance(type);
+        }
+    }
+}
---
+++ src/EulerSolver/Program.cs
@@ -0,0 +1,38 @@
+﻿using EulerSolver.Problems;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            execute();
+        }
+
+        static void execute()
+        {
+            Console.WriteLine("Inform the problem number to be resolved or type 'exit' to close");
+            var problemNumber = Console.ReadLine();
+            if (problemNumber == "exit")
+                return;
+
+            var problem = MagicActivator.MagicallyCreateInstance("_" + problemNumber);
+            if (problem != null)
+            {
+                var result = problem.Execute();
+                Console.WriteLine("Problem: " + problemNumber);
+                Console.WriteLine("Result: " + result);
+                Console.ReadKey();
+            }
+            else
+                Console.WriteLine("Problem not found");
+
+            execute();
+        }
+    }
+}
