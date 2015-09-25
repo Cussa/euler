afa12f1be718444091278830a1951d45ca33aba1
Cussa Mitre <cussa.mitre@gmail.com>
2015-09-25 15:46:50 -0300

    Problem 002

---
+++ src/EulerSolver/Problems/002.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolver.Problems
+{
+    class _002 : IEulerProblem
+    {
+        public string Execute()
+        {
+            var f1 = 1;
+            var f2 = 2;
+            var f3 = f2 + f1; ;
+            var result = f2;
+            while (f3 < 4000000)
+            {
+                if (f3 % 2 == 0)
+                {
+                    result += f3;
+                }
+
+                f1 = f2;
+                f2 = f3;
+                f3 = f2 + f1;
+            }
+
+            return result.ToString();
+        }
+    }
+}
