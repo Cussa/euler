9f2f15a777306a4eefd7c064cd137c16a9180868
Cussa Mitre <cussa.mitre@gmail.com>
2015-09-30 10:49:54 -0300

    Problem 005
    Implementation

---
+++ src/EulerSolver/Problems/005.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EulerSolver.Problems
+{
+    class _005 : EulerProblem
+    {
+        public override string Execute()
+        {
+            var ret = string.Empty;
+            var searching = true;
+            var numberTest = 20;
+            var modResult = 0;
+
+            //232792560
+            while (searching)
+            {
+                for (int i = 1; i <= 20; i++)
+                {
+                    modResult += numberTest % i;
+                    if (modResult > 0)
+                        break;
+                }
+
+                if (modResult > 0)
+                {
+                    numberTest += 20;
+                    modResult = 0;
+                    continue;
+                }
+
+                searching = false;
+                ret = numberTest.ToString();
+            }
+
+            return ret;
+        }
+    }
+}
